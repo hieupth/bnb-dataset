e9fb25dc1e3601f0b257abd80d081fea6976b9f4
hieupth <64821726+hieupth@users.noreply.github.com>
2024-08-15 14:35:56 +0700

    chore: fix cities fetch

--- cities.py
+++ cities.py
@@ -5,12 +5,15 @@ You should adapt this script according to the page you want to scrap
 from pathlib import Path
 import tap
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import numpy as np
 
 
 class Arguments(tap.Tap):
     url: str = "https://en.wikipedia.org/wiki/List_of_the_most_common_U.S._place_names"
     output: Path = "data/cities.txt"
-    xpath: str = '//table[@class="wikitable sortable jquery-tablesorter"]//td//a'
+    xpath: str = '//table[@class="wikitable sortable jquery-tablesorter"]/tbody/tr/td/a'
+    # xpath: str = '//*[@id="mw-content-text"]/div[1]/table[2]/tbody/tr/td/a'
 
 
 if __name__ == "__main__":
@@ -20,10 +23,13 @@ if __name__ == "__main__":
     driver = webdriver.Firefox()
     driver.get(args.url)
 
-    cities = [
-        a.text
-        for a in driver.find_elements_by_xpath(args.xpath)
-    ]
+    # cities = [
+    #     a.text
+    #     # for a in driver.find_elements_by_xpath(args.xpath)
+    #     for a in driver.find_element(By.XPATH, args.xpath)
+    # ]
+    cities = driver.find_elements(By.CSS_SELECTOR, "td > a")
+    cities = np.unique([x.accessible_name for x in cities]).tolist()
     driver.close()
 
     args.output.parent.mkdir(exist_ok=True, parents=True)
